c9bc6c1d259e0e66163d3b7fa11a3f5fdde6d49d
Evan Coury <me@evancoury.com>
2012-07-11 09:04:47 -0700

    add options class

---
+++ src/EdpGithub/Options.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace EdpGithub;
+
+use Zend\Stdlib\AbstractOptions;
+
+class Options extends AbstractOptions
+{
+    /**
+     * @var bool
+     */
+    protected $enableZfcUserAuthAdapter = false;
+
+    /**
+     * @var string
+     */
+    protected $githubClientId;
+
+    /*
+     * @var string
+     */
+    protected $githubClientSercret;
+
+    /*
+     * @var string
+     */
+    protected $githubOauthCallbackUrl;
+
+    /**
+     * Get enableZfcUserAuthAdapter.
+     *
+     * @return bool
+     */
+    public function getEnableZfcUserAuthAdapter()
+    {
+        return $this->enableZfcUserAuthAdapter;
+    }
+
+    /**
+     * Set enableZfcUserAuthAdapter.
+     *
+     * @param bool $enableZfcUserAuthAdapter
+     * @return Opions
+     */
+    public function setEnableZfcUserAuthAdapter($enableZfcUserAuthAdapter)
+    {
+        $this->enableZfcUserAuthAdapter = $enableZfcUserAuthAdapter;
+        return $this;
+    }
+
+    /**
+     * Get githubClientId.
+     *
+     * @return string.
+     */
+    public function getGithubClientId()
+    {
+        return $this->githubClientId;
+    }
+
+    /**
+     * Set githubClientId.
+     *
+     * @param string $githubClientId
+     * @return Options
+     */
+    public function setGithubClientId($githubClientId)
+    {
+        $this->githubClientId = $githubClientId;
+        return $this;
+    }
+
+    /**
+     * Get githubClientSercret.
+     *
+     * @return string
+     */
+    public function getGithubClientSercret()
+    {
+        return $this->githubClientSercret;
+    }
+
+    /**
+     * Set githubClientSercret.
+     *
+     * @param string $githubClientSercret
+     * @return Options
+     */
+    public function setGithubClientSercret($githubClientSercret)
+    {
+        $this->githubClientSercret = $githubClientSercret;
+        return $this;
+    }
+
+    /**
+     * Get githubOauthCallbackUrl.
+     *
+     * @return string
+     */
+    public function getGithubOauthCallbackUrl()
+    {
+        return $this->githubOauthCallbackUrl;
+    }
+
+    /**
+     * Set githubOauthCallbackUrl.
+     *
+     * @param string $githubOauthCallbackUrl
+     * @return Options
+     */
+    public function setGithubOauthCallbackUrl($githubOauthCallbackUrl)
+    {
+        $this->githubOauthCallbackUrl = $githubOauthCallbackUrl;
+        return $this;
+    }
+}
