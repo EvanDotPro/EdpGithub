7432a720c86313d175e67f2f680112c35795abb4
Hounddog <martin.shwalbe@gmail.com>
2012-10-26 19:33:42 +0200

    Adding Tests and Travis

---
+++ tests/EdpGithubTest/Api/CurrentUserTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace EdpGithubTest\Api;
+
+use PHPUnit_Framework_TestCase;
+
+class CurrentUserTest extends PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        // your code here
+    }
+
+    public function tearDown()
+    {
+        // your code here
+    }
+}
---
+++ tests/EdpGithubTest/Api/PullRequestTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace EdpGithubTest\Api;
+
+use PHPUnit_Framework_TestCase;
+
+class PullRequestTest extends PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        // your code here
+    }
+
+    public function tearDown()
+    {
+        // your code here
+    }
+}
