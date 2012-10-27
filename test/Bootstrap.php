26b1d51df00b2ec2d8b88ad6d9dee76a2dc1552e
Hounddog <martin.shwalbe@gmail.com>
2012-10-27 18:17:25 +0200

    Fixed autoloading in Bootstrap

--- test/Bootstrap.php
+++ test/Bootstrap.php
@@ -79,15 +79,17 @@ class Bootstrap
             }
 
             include $zf2Path . '/Zend/Loader/AutoloaderFactory.php';
-            AutoloaderFactory::factory(array(
-                'Zend\Loader\StandardAutoloader' => array(
-                    'autoregister_zf' => true,
-                    'namespaces' => array(
-                        __NAMESPACE__ => __DIR__ . '/' . __NAMESPACE__,
-                    ),
-                ),
-            ));
+
         }
+
+        AutoloaderFactory::factory(array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'autoregister_zf' => true,
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/' . __NAMESPACE__,
+                ),
+            ),
+        ));
     }
 
     protected static function findParentPath($path)
