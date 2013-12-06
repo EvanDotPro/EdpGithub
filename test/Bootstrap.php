063bd16662c7bc3372ff62bfe81a5a3a5067bffe
Martin <martin.shwalbe@gmail.com>
2013-12-06 16:02:00 +0100

    Modified testing behaviour

--- test/Bootstrap.php
+++ test/Bootstrap.php
@@ -1,14 +1,22 @@
 <?php
-/**
-* EdpGithub
-*
-*/
 
-if (
-    !($loader = @include __DIR__ . '/../vendor/autoload.php')
-    && !($loader = @include __DIR__ . '/../../../autoload.php')
-) {
+ini_set('error_reporting', E_ALL | E_STRICT);
+
+$files = array(__DIR__ . '/../vendor/autoload.php', __DIR__ . '/../../../autoload.php');
+
+foreach ($files as $file) {
+    if (file_exists($file)) {
+        $loader = require $file;
+
+        break;
+    }
+}
+
+if (! isset($loader)) {
     throw new RuntimeException('vendor/autoload.php could not be found. Did you run `php composer.phar install`?');
 }
 
+/* @var $loader \Composer\Autoload\ClassLoader */
 $loader->add('EdpGithubTest\\', __DIR__);
+
+unset($files, $file, $loader);
